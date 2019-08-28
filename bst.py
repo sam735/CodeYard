8c0bd61a36ce9e18ce7490a54e93f9ade2c05091
sanjeev <sanjeev@www.toolkit1.com>
2019-08-28 12:38:07 +0530

    Added some coment

--- bst.py
+++ bst.py
@@ -1,64 +1,67 @@
 from queue import SimpleQueue
 
+
 class Bst():
-    def __init__(self, data:int, l=None, r=None):
+    def __init__(self, data: int, l=None, r=None):
         self.data = data
         self.l = l
         self.r = r
 
-def insert_bst(data:int, root=None):
-    if root == None:
+
+def insert_bst(data: int, root=None):
+    if root is None:
         root = Bst(data)
         return root
-    
-    if(root.data> data):
-        root.l = insert_bst(data,root.l)
-    
-    if root.data<data:
-        root.r = insert_bst(data,root.r)
+    if(root.data > data):
+        root.l = insert_bst(data, root.l)
+    if root.data < data:
+        root.r = insert_bst(data, root.r)
     return root
 
+
 def in_oredr_traversal(root=None):
-    if(root == None):
+    if root is None:
         return
     in_oredr_traversal(root.l)
     print(root.data)
     in_oredr_traversal(root.r)
+
 
 def pre_order_traversal(root=None):
-    if(root == None):
+    if root is None:
         return
     print(root.data)
     in_oredr_traversal(root.l)
     in_oredr_traversal(root.r)
 
+
 def post_order_traversal(root=None):
-    if(root == None):
+    if root is None:
         return
     post_order_traversal(root.l)
     post_order_traversal(root.r)
     print(root.data)
 
+
 def level_order_traversal(root=None):
-    if root == None:
+    if root is None:
         return
     Que = SimpleQueue()
     Que.put(root)
-    while(Que.empty() == False):
+    while Que.empty() is False:
         node = Que.get()
         print(node.data)
-        if node.l !=None:
+        if node.l is not None:
             Que.put(node.l)
-        if node.r != None:
+        if node.r is not None:
             Que.put(node.r)
-    
 
 
 if __name__ == '__main__':
     root = None
-    for i in range(1,5):
+    for i in range(1, 5):
         n = int(input('Enter data:'))
-        root = insert_bst(n,root)
+        root = insert_bst(n, root)
     print('Inorder traversal')
     in_oredr_traversal(root)
     print('preorder traversal')
--- heap.py
+++ heap.py
@@ -1,20 +1,22 @@
-from heapq import heappop, heappush, heapify 
+from heapq import heappop, heappush, heapify
 
 
-arr = [2, 6, 3, 12, 56, 8] 
+arr = [2, 6, 3, 12, 56, 8]
+
 
 def heap_sort():
     heap = arr[:3+1]
     heapify(heap)
     new_arr = []
-    for i in range(4,len(arr)):
+    for i in range(4, len(arr)):
         new_arr.append(heappop(heap))
-        heappush(heap,arr[i])
+        heappush(heap, arr[i])
 
     while heap:
         new_arr.append(heappop(heap))
-    
     return new_arr
+
+
 if __name__ == "__main__":
-   arr = heap_sort()
-   print(arr)
+    arr = heap_sort()
+    print(arr)
